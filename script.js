3ac24d19798349931fb1fff32be8fc26e965ce8e
Po0rFish <55439560+Po0rFish@users.noreply.github.com>
2020-08-03 18:24:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// const result = confirm('do you have 18?', '18');
+// console.log(result);
+
+// const answer = +prompt('do you have 18?','');
+// console.log(answer + 5);
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?','');
+// answers[1] = prompt('Сколько вам лет?','');
+// answers[2] = prompt('Как ваша фамилия?','');
+
+console.log(typeof(answers));
+
+let incr = 10,
+    decr = 10;
+incr ++;
+decr --;
+
+console.log(incr);
+console.log(decr);
